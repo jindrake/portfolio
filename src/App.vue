3f355562e37076aca54ec367c6ca87a6873510e1
Jethro Tanjay <tanjay.jethro@gmail.com>
2018-06-28 08:36:17 +0800

    build: init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <profile-card/>
+  </div>
+</template>
+
+<script>
+import ProfileCard from './components/ProfileCard'
+
+export default {
+  name: 'App',
+  components: {
+    ProfileCard
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    Hello World
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'profile-card',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card {
+  color: #42b983;
+  background-color: black;
+  height: 100%;
+  width: 40%;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+}
+</style>
